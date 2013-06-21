2c20b2beb954b2dc24f456aebce1a6c9fe3355ff
ggarciabas <ggarciabas@gmail.com>
2013-06-21 14:55:58 -0300

    Adicionado novos atributos.
    Giovanna Garcia Basilio - 210613

--- Controle Salas - UTFPr (Console)/src/modelo/Pessoa.java
+++ Controle Salas - UTFPr (Console)/src/modelo/Pessoa.java
@@ -14,6 +14,8 @@
  */
 package modelo;
 
+import java.util.Date;
+
 /**
  *  Classe Pessoa
  *  
@@ -22,11 +24,34 @@ package modelo;
  */
 public abstract class Pessoa {
 
-    private String m_nome;
+    protected String m_nome;
+    protected String m_login;
+    protected String m_senha;
+    protected Date m_inicio;
     
     public String getNome () {
         return this.m_nome;
     }
+
+    public String getM_login() {
+        return m_login;
+    }
+
+    public String getM_senha() {
+        return m_senha;
+    }
+
+    public void setM_nome(String m_nome) {
+        this.m_nome = m_nome;
+    }
+
+    public void setM_login(String m_login) {
+        this.m_login = m_login;
+    }
+
+    public void setM_senha(String m_senha) {
+        this.m_senha = m_senha;
+    }        
     
     public abstract String getDescricao ();
 }

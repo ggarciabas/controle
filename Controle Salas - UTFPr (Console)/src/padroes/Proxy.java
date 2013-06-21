746d37d5168b1b0b06e9088bb73be96769674edc
ggarciabas <ggarciabas@gmail.com>
2013-06-21 14:57:26 -0300

    Adicionado a classe responsável pelo controle dos gerenciamentos. PRoxy Pattern.
    Giovanna Garcia Basilio - 210613

---
+++ Controle Salas - UTFPr (Console)/src/padroes/Proxy.java
@@ -0,0 +1,82 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package padroes;
+
+import enumeracoes.TipoPessoa;
+import modelo.Pessoa;
+import modelo.Sala;
+import modelo.concreto.Reserva;
+import padroes.interfaces.GerenciarPessoa;
+import padroes.interfaces.GerenciarReservas;
+import padroes.interfaces.GerenciarSala;
+
+/**
+ *  Class Proxy
+ *      Define o padrão proxy.
+ *      O proxy deverá ser único para cada pessoa logada no sistema. ** Juliano
+ *  
+ * @author Giovanna Garcia
+ * @author Marcos Agnes
+ */
+public class Proxy implements GerenciarPessoa, GerenciarReservas, GerenciarSala {
+    
+    private Pessoa m_pessoa = null;
+    private Sala m_sala = null;
+
+    public Proxy(Pessoa m_pessoa) {
+        this.m_pessoa = m_pessoa;
+    }        
+
+    @Override
+    public Pessoa criarPessoa(TipoPessoa tp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletarPessoa(Pessoa p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alterarPessoa(Pessoa p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Reserva criarReserva(Pessoa p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void liberarReserva() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Sala criarSala() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletarSala(Sala s) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alterarSala(Sala s) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
